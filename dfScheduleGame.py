1f355b638985373e869c26c87e8a668a8e012532
Zion <jli21bh@gmail.com>
2022-07-11 13:23:35 -0500

    initialized variables

--- dfScheduleGame.py
+++ dfScheduleGame.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import datetime
 import matplotlib.pyplot as plt
 import bs4 as bs
 from urllib.request import urlopen
@@ -11,44 +12,13 @@ from sportsreference.nba.roster import Roster
 from sportsreference.nba.roster import Player
 
 from sportsreference.nba.schedule import Schedule
-from sportsreference.nba.schedule import Boxscore
+from sportsipy.nba.boxscore import Boxscores
+
+
+
+listGames = Boxscores(datetime(2021, 10, 19))
 
 
 teamSchedule = Schedule('GSW', year = 2022).dataframe
 teamSchedule.to_csv('teamSchedule.csv', index = False)
-gamedata = Boxscore
 
-teams2022 = Teams(year = '2022')
-ids = []
-names = []
-heights = []
-weights = []
-roles = []
-salary = []
-shoots = []
-
-for team in teams2022:
-    teamDict = Roster(team.abbreviation, year='2022',
-                      slim=True).players
-    for key, value in teamDict.items():
-        if key not in ids:
-            ids.append(key)
-            names.append(value)
-            salary.append(Player(key).salary)
-            url = f"https://www.basketball-reference.com/players/{value.split(' ')[1][0:1].lower()}/{key}.html"
-            print(url)
-            soup = bs.BeautifulSoup(urlopen(url), features='lxml')
-            stats = [p.getText() for p in soup.find('div', {'id':'meta'}).findAll('p')]
-            print(stats[4])
-            raise Exception('aaahhh')
-            
-
-df = pd.DataFrame()
-df['Name'] = names
-df['Id'] = ids
-df['height'] = heights
-df['weights'] = weights
-df['role'] = roles
-df['salary'] = salary
-df['shoots'] = shoots
-        
