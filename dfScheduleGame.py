97383ffb0e14c3ba4453814653b38000f236b76c
Zion <jli21bh@gmail.com>
2022-07-21 20:34:37 -0400

    Update dfScheduleGame.py

--- dfScheduleGame.py
+++ dfScheduleGame.py
@@ -18,86 +18,63 @@ from sportsipy.nba.boxscore import Boxscore
 from sportsipy.nba.boxscore import Boxscores
 import re
 
-#listGames = 0
-#listGames = Boxscores(date(2021, 10, 19), date(2021, 10, 19)).games
-
-#i = 0
-#gameDay = '10-19-2021'
-#game = listGames[gameDay][i]
-
-#gameIdList = [game['boxscore'], game['boxscore']]
-#gameId = game['boxscore']
-
-
 def getGameData(gameId):
-    df = pd.DataFrame()
 
-    teams, gameIdList, q1Score, q2Score, q3Score, q4Score, points, location, daysSinceLastGame, gamesInPastWeek, timeOfDay, roster, coach, record, winsAgainstTeam, streak = [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []
+    '''
+    Returns a 1 by x array of relevant game data given a game Id.
 
+    '''
     if bool(re.match("^[\d]{9}[A-Z]{3}$", gameId)):
         gameYear = gameId[0:4]
         gameMonth = gameId[4:6]
         gameDay = gameId[6:8]
-        gameDate = gameYear + '-' + gameMonth + '-' + gameDay
+        gameDate = gameYear + ', ' + gameMonth + ', ' + gameDay
         gamesToday = list(Boxscores(dt.datetime.strptime(gameDate, '%Y, %m, %d')).games.values())[0]
         a = next(item for item in gamesToday if item["boxscore"] == gameId)
-        home_abbr = a['home_abbr']
-        away_abbr = a['away_abbr']
-        teams = [home_abbr, away_abbr]
+        teamHome = a['home_abbr']
+        teamAway = a['away_abbr']
     else:
         raise Exception('Issue with Game ID')
     # wins against team is computed with past 5 years data
     gameData = Boxscore(gameId)
+    q1ScoreHome = gameData.summary['home'][0]
+    q1ScoreAway = gameData.summary['away'][0]
+    q2ScoreHome = gameData.summary['home'][1]
+    q2ScoreAway = gameData.summary['away'][1]
+    q3ScoreHome = gameData.summary['home'][2]
+    q3ScoreAway = gameData.summary['away'][2]
+    q4ScoreHome = gameData.summary['home'][3]
+    q4ScoreAway = gameData.summary['away'][3]
     
-    gameIdList = [gameId, gameId] 
-    q1Score = [gameData.summary['home'][0], gameData.summary['away'][0]]
-    q2Score = [gameData.summary['home'][1], gameData.summary['away'][1]]
-    q3Score = [gameData.summary['home'][2], gameData.summary['away'][2]]
-    q4Score = [gameData.summary['home'][3], gameData.summary['away'][3]]
-    points = [game['home_score'], game['away_score']]
-
+    pointsHome = gameData.away_points
+    pointsAway = gameData.away_points 
+    
     teamHomeSchedule = Schedule(teams[0], year=2022).dataframe
     teamAwaySchedule = Schedule(teams[1], year=2022).dataframe
-    timeOfDay = [teamHomeSchedule.loc[gameId][13], teamAwaySchedule.loc[gameId][13]]
-    streak = [teamHomeSchedule.loc[gameId][12], teamAwaySchedule.loc[gameId][12]]
-    # caution: streak might be included with the current loss/win
-
-    df['teams'] = teams
-    df['gameId'] = gameIdList
+    
+    timeOfDay = teamHomeSchedule.loc[gameId][13]
+    streakHome = teamHomeSchedule.loc[gameId][12]
+    streakAway = teamAwaySchedule.loc[gameId][12]
+    
+    # caution: streak is included with the current loss/win
 
     teamHomeSchedule.sort_values(by='datetime')
     teamAwaySchedule.sort_values(by='datetime')
 
-    daysSinceLastGame = []
-    gamesInPastWeek = []
-
-    prevdate = teamHomeSchedule['datetime'].shift().loc[gameId]
+    prevHomeDate = teamHomeSchedule['datetime'].shift().loc[gameId]
+    prevAwayDate = teamAwaySchedule['datetime'].shift().loc[gameId]
     currentdate = teamHomeSchedule.loc[gameId]['datetime']
-    daysSinceLastGame.append((currentdate - prevdate).total_seconds() / 86400)
-
-    temp = teamHomeSchedule[(teamHomeSchedule['datetime'] - currentdate).dt.total_seconds() < 86400 * 7]
-    temp = temp[temp['datetime'] < currentdate]
-    gamesInPastWeek.append(temp.shape[0])
-
-    prevdate = teamAwaySchedule['datetime'].shift().loc[gameIdList[0]]
-    daysSinceLastGame.append((currentdate - prevdate).total_seconds() / 86400)
-
-    temp = teamAwaySchedule[(teamAwaySchedule['datetime'] - currentdate).dt.total_seconds() < 86400 * 7]
-    temp = temp[temp['datetime'] < currentdate]
-    gamesInPastWeek.append(temp.shape[0])
-
-    roster = [gameData.home_players, gameData.away_players]
     
+    daysSinceLastGameHome = (currentdate - prevHomeDate).total_seconds() / 86400
+    daysSinceLastGameAway = (currentdate - prevAwayDate).total_seconds() / 86400
     homePlayerRoster = [player.player_id for player in gameData.home_players]
     awayPlayerRoster = [player.player_id for player in gameData.away_players]
-    roster = [homePlayerRoster, awayPlayerRoster]
 
     '''
     Gets the name of coaches based on year of game day from https://www.basketball-reference.com/teams/.
     '''
     urlHome = f"https://www.basketball-reference.com/teams/{home_abbr}/{gameDate[:4].lower()}.html"
     try:
-        #print(urlHome)
         page = requests.get(urlHome)
         doc = html.fromstring(page.content)
         homeCoach = doc.xpath('//*[@id="meta"]/div[2]/p[2]/a/text()')
@@ -107,22 +84,21 @@ def getGameData(gameId):
     urlAway = f"https://www.basketball-reference.com/teams/{away_abbr}/{gameDate[:4].lower()}.html"
 
     try:
-        #print(urlAway)
         page = requests.get(urlAway)
         doc2 = html.fromstring(page.content)
         awayCoach = doc2.xpath('//*[@id="meta"]/div[2]/p[2]/a/text()')
     except:
         raise Exception('Coach not found on basketball-reference.com for ' + Teams()(away_abbr).name)
 
-    coach = [homeCoach[0], awayCoach[0]]
 
-    location = [gameData.location, gameData.location]
+    location = gameData.location
 
     '''
     Find records of home and away teams by searching schedule arrays of home and 
     away team respectively
     '''
-    #Calculating Home Record
+    
+    # Calculating Home Record
     if (int(gameMonth.lstrip("0")) > 6): #converted gameMonth to int without leading 0. check month to find correct season
         teamHomeSchedule = Schedule(teams[0], int(gameYear) + 1).dataframe
     else:
@@ -130,7 +106,7 @@ def getGameData(gameId):
 
     homeResults = teamHomeSchedule.result #only show results column
     homeResults = homeResults.loc[homeResults.index[0]:gameId] #only show up to current game row
-    homeRecord = homeResults.value_counts(ascending=True) #sorts strings 'Win' and 'Loss' alphabetically which makes homeRecord[1] Wins and homeRecord[0] Losses
+    homeRecord = homeResults.value_counts(ascending = True) #sorts strings 'Win' and 'Loss' alphabetically which makes homeRecord[1] Wins and homeRecord[0] Losses
     try:
         homeWins = homeRecord[1]
     except:
@@ -147,7 +123,7 @@ def getGameData(gameId):
         teamAwaySchedule = Schedule(teams[1], int(gameYear)).dataframe
     awayResults = teamAwaySchedule.result
     awayResults = awayResults.loc[awayResults.index[0]:gameId]
-    awayRecord = awayResults.value_counts(ascending=True)
+    awayRecord = awayResults.value_counts(ascending = True)
     try:
         awayWins = awayRecord[1]
     except:
@@ -157,30 +133,31 @@ def getGameData(gameId):
     except:
         awayLosses = 0
 
-    record = [[homeWins, homeLosses], [awayWins, awayLosses]]
+    homeRecord = [homeWins, homeLosses]
+    awayRecord = [awayWins, awayLosses]
 
-    tempDf = teamHomeSchedule.loc[teamHomeSchedule['opponent_abbr'] == game['away_abbr']]
+    tempDf = teamHomeSchedule.loc[teamHomeSchedule['opponent_abbr'] == teamHome]
     tempDf = tempDf.loc[teamHomeSchedule['datetime'] < currentdate]
+    
     homeTeamMatchupWins = tempDf.loc[teamHomeSchedule['result'] == 'Win'].shape[0]
-    homeTeamMatchupLosses = tempDf.loc[teamHomeSchedule['result'] == 'Loss'].shape[0]
+    awayTeamMatchupWins = tempDf.loc[teamHomeSchedule['result'] == 'Loss'].shape[0]
+            
+    gameData = [gameId, teamHome, teamAway, timeOfDay, location, q1ScoreHome, q2ScoreHome, q3ScoreHome, q4ScoreHome, pointsHome, streakHome, daysSinceLastGameHome, homePlayerRoster, homeRecord, homeTeamMatchupWins, q1ScoreAway, q2ScoreAway, q3ScoreAway, q4ScoreAway, pointsAway, streakAway, daysSinceLastGameAway, awayPlayerRoster] 
+    
+    return gameData
 
-    winsAgainstTeam = [homeTeamMatchupWins, homeTeamMatchupLosses]
-
-    df['teams'] = teams
-    df['gameId'] = gameIdList
-    df['q1Score'] = q1Score
-    df['q2Score'] = q2Score
-    df['q3Score'] = q3Score
-    df['q4Score'] = q4Score
-    df['points'] = points
-    df['location'] = location
-    df['daysSinceLastGame'] = daysSinceLastGame
-    df['gamesInPastWeek'] = gamesInPastWeek
-    df['timeOfDay'] = timeOfDay
-    df['roster'] = roster
-    # df['coach'] = coach
-    df['record'] = record
-    df['winsAgainstTeam'] = winsAgainstTeam
-
-    df.set_index(['teams'])
-    return df
+def getGameDataframe(startTime, endTime):
+    '''
+    startTime and endTime must be in format '%Y, %m, %d'
+    
+    '''
+    df = pd.DataFrame()
+    allGames = Boxscores(dt.datetime.strptime(startTime, '%Y, %m, %d'), dt.datetime.strptime(endTime, '%Y, %m, %d')).games
+    gameIdList = [] 
+    for key in allGames.keys():
+        print(key)
+        for i in range(len(allGames[key])):
+            gameIdList.append(allGames[key][i]['boxscore'])
+    
+    return df 
+    
