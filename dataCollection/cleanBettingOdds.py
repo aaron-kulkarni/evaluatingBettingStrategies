763c54d364b81c07803f70c776d42caf6f81a2f5
Zion <jli21bh@gmail.com>
2022-08-08 16:00:23 -0400

    updated cleanBettingOdds

--- dataCollection/cleanBettingOdds.py
+++ dataCollection/cleanBettingOdds.py
@@ -24,6 +24,7 @@ def convertBettingOdds(filename):
     1. Removes games that are not in regular season
     2. Adds GameID
     3. Checks if all games in betting odds file are in regular season and vice versa
+    4. Pivots table
     """
 
     year = re.findall('[0-9]+', filename)[0]
@@ -40,24 +41,24 @@ def convertBettingOdds(filename):
     df.sort_values(by='Date', ascending=True, inplace=True)
 
     # IMPORTANT: MUST CHANGE FOR SPECIFIC FILE WHERE CLEAN DATA FOR CORRESPONDING YEAR IS LOCATED 
-    # fileLocation = '../data/gameStats/game_data_player_stats_{}.csv'.format(year)
+    fileLocation = '../data/gameStats/game_data_player_stats_{}.csv'.format(year)
 
-    yeardates = {
-        2015: ('2014-10-28', '2015-06-15'),
-        2016: ('2015-10-27', '2016-06-19'),
-        2017: ('2016-10-25', '2017-06-12'),
-        2018: ('2017-10-17', '2018-06-08'),
-        2019: ('2018-10-16', '2019-06-13'),
-        2020: ('2019-10-22', '2020-10-11'),
-        2021: ('2020-12-22', '2021-07-20'),
-        2022: ('2021-10-19', '2022-06-16')
-    }
+    # yeardates = {
+      #  2015: ('2014-10-28', '2015-06-15'),
+      #  2016: ('2015-10-27', '2016-06-19'),
+      #  2017: ('2016-10-25', '2017-06-12'),
+      #  2018: ('2017-10-17', '2018-06-08'),
+      #  2019: ('2018-10-16', '2019-06-13'),
+      #  2020: ('2019-10-22', '2020-10-11'),
+      #  2021: ('2020-12-22', '2021-07-20'),
+      #  2022: ('2021-10-19', '2022-06-16')
+    # }
 
-    # startDate = str(extract_lines(fileLocation)[0])[0:10]
-    # endDate = str(extract_lines(fileLocation)[1])[0:10]
+    startDate = str(extract_lines(fileLocation)[0])[0:10]
+    endDate = str(extract_lines(fileLocation)[1])[0:10]
 
-    startDate = yeardates[int(year)][0]
-    endDate = yeardates[int(year)][1]
+    # startDate = yeardates[int(year)][0]
+    # endDate = yeardates[int(year)][1]
 
     df = df[(df['Date'] >= startDate) & (df['Date'] <= endDate)]
     df['game_id'] = df.apply(lambda d: str(d['Date'])[0:10].replace('-', '') + '0' + teamDict[d['Home_id']], axis=1)
@@ -80,14 +81,24 @@ def convertBettingOdds(filename):
     df.drop('Score_home', inplace=True, axis=1)
     df.drop('Score_away', inplace=True, axis=1)
     df.drop('Date', inplace=True, axis=1)
-    df.set_index(['game_id', 'Bookmaker'], inplace=True)
+    
+    df = df.pivot(index = 'game_id', columns = 'Bookmaker', values = ['OddHome', 'OddAway'])
+    
     return df
 
 
 def cleanBettingOdds(filename):
-    print('Not Implemented:', filename)
+    '''
+    1. Verifies that OddHome and OddAway are different signs
+    2. Returns dataframe that displays betting odd accruacy of each betting bookmaker
+    '''
+    year = re.findall('[0-9]+', filename)[0]
+    oddsDF = pd.read_csv('../data/bettingOddsData/closing_betting_odds_{0}_clean.csv'.format(year), header = [0,1], skipinitialspace = True)
+
+    return df 
 
 
+    
 years = np.arange(2015, 2023)
 for y in years:
     convertBettingOdds('../data/bettingOddsData/closing_betting_odds_{0}_FIXED.csv'.format(y)) \
