fixed average_team stats

dataProcessing/teamPerformance.py
@@ -28,10 +28,6 @@ def fixGameStateData(filename):
     df['gameState', 'rivalry'] = gameState['rivalry']
     df.drop(['homeProb', 'awayProb', 'homeProbAdj', 'awayProbAdj'], inplace = True, axis = 1)
     return df
-
-#years = np.arange(2015, 2023)
-#for year in years:
-    #fixGameStateData('../data/gameStats/game_state_data_{}.csv'.format(year)).to_csv('game_state_data_new_{}.csv'.format(year))
         
 def getTeamSchedule(team, year):
 
@@ -100,6 +96,25 @@ def plotValues(team, year, cumulative = True):
         df = df.expanding().mean()
     return df.array
 
+def fixRecentStats(year):
+    df = pd.read_csv('data/averageTeamData/average_team_per_5_{}.csv'.format(year), header = [1])
+    #dfPlayer['MP'] = dfPlayer.apply(lambda d: round(float(d['MP'][0:2]) + (float(d['MP'][3:5])/60), 2), axis = 1)
+    #df['NanN'] = df.apply(lambda d: )
+    #df['<= 53'] = df['mynumbers'].apply(lambda x: 'True' if x <= 53 else 'False')
+    #df['Unnamed: 0'] = df['Unnamed: 0'].apply(lambda d: '' if d == 0 else d)
+    #df['gameId'] = df['gameId'].apply(lambda x: '' if x == 'NaN' else x)
+    df['firstCol'] = df['Unnamed: 0'].apply(lambda d: '' if d == '0' else d)
+    df['secondCol'] = df['gameId'].apply(lambda x: '' if type(x) == float else x)
+    df['fixed'] = df['firstCol'] + df['secondCol']
+    del df['Unnamed: 0']
+    del df['gameId']
+    del df['firstCol']
+    del df['secondCol']
+    df.set_index('fixed', inplace = True)
+    df.index.names = ['gameId']
+
+    return df
+
 # rate = []
 # for team in Teams():
 #     teamAbbr = re.search(r'\((.*?)\)', str(team)).group(1)
@@ -111,5 +126,9 @@ def plotValues(team, year, cumulative = True):
 
 # #plt.plot(x, rate)
 # plt.show()
+
+# years = np.arange(2015, 2022)
+# for year in years:
+#     fixRecentStats(year).to_csv('average_team_per_5_{}.csv'.format(year))
    
     