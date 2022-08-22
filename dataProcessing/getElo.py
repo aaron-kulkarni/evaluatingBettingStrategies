263338ded8d9ce0a50b0e386979fe3d7c15fcfae
aaron-kulkarni <aaron.d.kulkarni@gmail.com>
2022-08-22 17:15:33 -0500

    added all beginning of season elo ratings

--- dataProcessing/getElo.py
+++ dataProcessing/getElo.py
@@ -64,52 +64,269 @@ def getElo(year):
 
     if year == 2015:
         eloDict = {
-            'ATL' : 
-            'BOS' :
-            'BKN' :
-            'CHA' :
-            'CHI' :
-            'CLE' :
-            'DAL' :
-            'DEN' :
-            'DET' :
-            'GSW' :
-            'HOU' :
-            'IND' :
-            'LAC' :
-            'LAL' :
-            'MEM' :
-            'MIA' :
-            'MIL' :
-            'MIN' :
-            'NOP' :
-            'NYK' :
-            'OKC' :
-            'ORL' :
-            'PHI' :
-            'PHX' :
-            'POR' :
-            'SAC' :
-            'SAS' :
-            'TOR' :
-            'UTA' :
-            'WSH' :
-}
+            'ATL' : 1490,
+            'BOS' : 1379,
+            'BKN' : 1518,
+            'CHA' : 1511,
+            'CHI' : 1547,
+            'CLE' : 1464,
+            'DAL' : 1592,
+            'DEN' : 1473,
+            'DET' : 1389,
+            'GSW' : 1591,
+            'HOU' : 1596,
+            'IND' : 1533,
+            'LAC' : 1631,
+            'LAL' : 1422,
+            'MEM' : 1578,
+            'MIA' : 1579,
+            'MIL' : 1318,
+            'MIN' : 1513,
+            'NOP' : 1457,
+            'NYK' : 1534,
+            'OKC' : 1620,
+            'ORL' : 1359,
+            'PHI' : 1316,
+            'PHX' : 1560,
+            'POR' : 1568,
+            'SAC' : 1431,
+            'SAS' : 1700,
+            'TOR' : 1551,
+            'UTA' : 1373,
+            'WSH' : 1541
+        }
 
-    if year == 2016:
-        eloDict = {}
-    if year = 2017:
-        eloDict = {}
-    if year = 2018:
-        eloDict = {}
-    if year = 2019:
-        eloDict = {}    
-    if year = 2020:
-        eloDict = {}
-    if year = 2021:
-        eloDict = {}
-    if year = 2022:
-        eloDict = {}
+    elif year == 2016:
+        eloDict = {
+            'ATL' : 1562,
+            'BOS' : 1520,
+            'BKN' : 1470,
+            'CHA' : 1427,
+            'CHI' : 1570,
+            'CLE' : 1645,
+            'DAL' : 1544,
+            'DEN' : 1443,
+            'DET' : 1472,
+            'GSW' : 1743,
+            'HOU' : 1617,
+            'IND' : 1505,
+            'LAC' : 1647,
+            'LAL' : 1339,
+            'MEM' : 1583,
+            'MIA' : 1468,
+            'MIL' : 1459,
+            'MIN' : 1324,
+            'NOP' : 1521,
+            'NYK' : 1318,
+            'OKC' : 1564,
+            'ORL' : 1360,
+            'PHI' : 1333,
+            'PHX' : 1476,
+            'POR' : 1544,
+            'SAC' : 1440,
+            'SAS' : 1667,
+            'TOR' : 1502,
+            'UTA' : 1543,
+            'WSH' : 1536
+        }
+    elif year == 2017:
+        eloDict = {
+            'ATL' : 1571,
+            'BOS' : 1540,
+            'BKN' : 1343,
+            'CHA' : 1546,
+            'CHI' : 1467,
+            'CLE' : 1696,
+            'DAL' : 1503,
+            'DEN' : 1446,
+            'DET' : 1497,
+            'GSW' : 1693,
+            'HOU' : 1528,
+            'IND' : 1533,
+            'LAC' : 1584,
+            'LAL' : 1333,
+            'MEM' : 1455,
+            'MIA' : 1574,
+            'MIL' : 1420,
+            'MIN' : 1434,
+            'NOP' : 1407,
+            'NYK' : 1415,
+            'OKC' : 1685,
+            'ORL' : 1454,
+            'PHI' : 1278,
+            'PHX' : 1393,
+            'POR' : 1585,
+            'SAC' : 1445,
+            'SAS' : 1695,
+            'TOR' : 1569,
+            'UTA' : 1530,
+            'WSH' : 1524
+        }
+    elif year == 2018:
+        eloDict = {
+            'ATL' : 1486,
+            'BOS' : 1532,
+            'BKN' : 1405,
+            'CHA' : 1473,
+            'CHI' : 1497,
+            'CLE' : 1648,
+            'DAL' : 1441,
+            'DEN' : 1540,
+            'DET' : 1457,
+            'GSW' : 1761,
+            'HOU' : 1574,
+            'IND' : 1503,
+            'LAC' : 1591,
+            'LAL' : 1401,
+            'MEM' : 1489,
+            'MIA' : 1553,
+            'MIL' : 1508,
+            'MIN' : 1474,
+            'NOP' : 1488,
+            'NYK' : 1407,
+            'OKC' : 1518,
+            'ORL' : 1390,
+            'PHI' : 1380,
+            'PHX' : 1381,
+            'POR' : 1531,
+            'SAC' : 1421,
+            'SAS' : 1617,
+            'TOR' : 1532,
+            'UTA' : 1580,
+            'WSH' : 1566
+        }
+    elif year == 2019:
+        eloDict = {
+            'ATL' : 1388,
+            'BOS' : 1562,
+            'BKN' : 1432,
+            'CHA' : 1502,
+            'CHI' : 1364,
+            'CLE' : 1559,
+            'DAL' : 1394,
+            'DEN' : 1567,
+            'DET' : 1492,
+            'GSW' : 1685,
+            'HOU' : 1654,
+            'IND' : 1555,
+            'LAC' : 1506,
+            'LAL' : 1491,
+            'MEM' : 1367,
+            'MIA' : 1499,
+            'MIL' : 1518,
+            'MIN' : 1537,
+            'NOP' : 1565,
+            'NYK' : 1410,
+            'OKC' : 1584,
+            'ORL' : 1378,
+            'PHI' : 1607,
+            'PHX' : 1334,
+            'POR' : 1560,
+            'SAC' : 1396,
+            'SAS' : 1540,
+            'TOR' : 1577,
+            'UTA' : 1623,
+            'WSH' : 1500
+        }
+    elif year == 2020:
+        eloDict = {
+            'ATL' : 1423,
+            'BOS' : 1578,
+            'BKN' : 1495,
+            'CHA' : 1497,
+            'CHI' : 1350,
+            'CLE' : 1350,
+            'DAL' : 1462,
+            'DEN' : 1586,
+            'DET' : 1476,
+            'GSW' : 1635,
+            'HOU' : 1653,
+            'IND' : 1510,
+            'LAC' : 1517,
+            'LAL' : 1473,
+            'MEM' : 1459,
+            'MIA' : 1499,
+            'MIL' : 1643,
+            'MIN' : 1465,
+            'NOP' : 1415,
+            'NYK' : 1319,
+            'OKC' : 1552,
+            'ORL' : 1543,
+            'PHI' : 1582,
+            'PHX' : 1338,
+            'POR' : 1602,
+            'SAC' : 1468,
+            'SAS' : 1554,
+            'TOR' : 1673,
+            'UTA' : 1596,
+            'WSH' : 1435
+        }
+    elif year == 2021:
+        eloDict = {
+            'ATL' : 1385,
+            'BOS' : 1646,
+            'BKN' : 1489,
+            'CHA' : 1424,
+            'CHI' : 1383,
+            'CLE' : 1363,
+            'DAL' : 1528,
+            'DEN' : 1557,
+            'DET' : 1382,
+            'GSW' : 1395,
+            'HOU' : 1541,
+            'IND' : 1527,
+            'LAC' : 1597,
+            'LAL' : 1646,
+            'MEM' : 1538,
+            'MIA' : 1603,
+            'MIL' : 1605,
+            'MIN' : 1393,
+            'NOP' : 1500,
+            'NYK' : 1384,
+            'OKC' : 1534,
+            'ORL' : 1495,
+            'PHI' : 1535,
+            'PHX' : 1555,
+            'POR' : 1511,
+            'SAC' : 1504,
+            'SAS' : 1529,
+            'TOR' : 1648,
+            'UTA' : 1542,
+            'WSH' : 1407
+        }
+    elif year == 2022:
+        eloDict = {
+            'ATL' : 1570,
+            'BOS' : 1500,
+            'BKN' : 1605,
+            'CHA' : 1438,
+            'CHI' : 1495,
+            'CLE' : 1353,
+            'DAL' : 1541,
+            'DEN' : 1577,
+            'DET' : 1381,
+            'GSW' : 1529,
+            'HOU' : 1346,
+            'IND' : 1487,
+            'LAC' : 1624,
+            'LAL' : 1549,
+            'MEM' : 1541,
+            'MIA' : 1513,
+            'MIL' : 1658,
+            'MIN' : 1439,
+            'NOP' : 1482,
+            'NYK' : 1548,
+            'OKC' : 1309,
+            'ORL' : 1330,
+            'PHI' : 1609,
+            'PHX' : 1650,
+            'POR' : 1581,
+            'SAC' : 1454,
+            'SAS' : 1478,
+            'TOR' : 1449,
+            'UTA' : 1615,
+            'WSH' : 1495
+        }
 
     gameIdList =  pd.read_csv('../data/gameStats/game_state_data_{}.csv'.format(year), header = [0,1], index_col = 0).index
     df = pd.DataFrame()
