22b2f5e8be1596404d27bc147d155d4ce215c25c
Sameer Narendran <snarendran@wisc.edu>
2022-08-27 17:41:23 -0400

    created getPreviousGameStats.py file template

---
+++ dataProcessing/getPreviousGameStats.py
@@ -0,0 +1,40 @@
+
+
+def getPreviousGamePlayerStats(game_id):
+    """
+    Gets all numerical stats in game_data_player_stats for games
+    that took place before (and not including) the given game, for each
+    player on the roster in the given game. Does not include player
+    statistics from when a player was benched the entire game.
+
+    Parameters
+    ----------
+    game_id : the basketball-reference.com id of the game
+
+    Returns
+    -------
+    a pandas dataframe with all the players' statistics for each
+    previous game.
+    """
+
+    print('Not Implemented:', game_id)
+
+
+def getPreviousGameTeamStats(game_id):
+    """
+    Gets all numerical stats in game_data_player_stats for games
+    that took place before (and not including) the given game, for each
+    team playing in the given game.
+
+    Parameters
+    ----------
+    game_id : the basketball-reference.com id of the game
+
+    Returns
+    -------
+    a pandas dataframe with all the players' statistics for each
+    previous game.
+    """
+
+    print('Not Implemented:', game_id)
+
