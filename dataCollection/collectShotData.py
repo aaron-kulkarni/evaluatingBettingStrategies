1c84dc3ff89315edfcdcc98b19c73c8355938fd4
aaron-kulkarni <aaron.d.kulkarni@gmail.com>
2022-09-04 16:17:40 -0500

    fully fixed shot charts and added playerids

--- dataCollection/collectShotData.py
+++ dataCollection/collectShotData.py
@@ -51,8 +51,10 @@ def scrapeGameShots(gameId):
     Returns
     -------
     A list as such: {
-        [homeTeamAbbr, [arrayOfDistanceValuesHome], [arrayOfResultValuesHome], awayTeamAbbr, [arrayOfDistanceValuesAway], [arrayOfResultValuesAway]]
-        arrayOfResultValues -> 0 for miss, 1 for make
+        [homeTeamAbbr, [arrayOfDistanceValuesHome], [arrayOfResultValuesHome], [arrayOfShotAnglesHome], [arrayofPlayerIdsHome],
+        awayTeamAbbr, [arrayOfDistanceValuesAway], [arrayOfResultValuesAway], [arrayofShotAnglesAway], [arrayofPlayerIdsAway]]
+
+        (arrayOfResultValues -> 0 for miss, 1 for make)
     }
     """
 
@@ -75,42 +77,52 @@ def scrapeGameShots(gameId):
         distances = []
         results = []
         angles = []
+        playerids = []
         homeShots = soup.find(id = 'shots-{}'.format(homeTeam))
         for shot in homeShots:
             if (shot != "\n" and shot.name != 'img'):
                 info = shot.attrs['tip'].split("<br>")[1].split(" ")
                 while (len(info) > 7): #if player has middle name or suffix(ex. marvin bagley III)
                     info = info[1:]
+                while (len(info) < 7): #if player has no last name(ex. Nene)
+                    info.insert(0, 'a')
                 distances.append(info[5])
                 if (info[2] == 'made'):
                     results.append(1)
                 else:
                     results.append(0)
-                positions = shot.attrs['style']
-                res = [int(s) for s in re.findall(r'-?\d+\.?\d*', positions)]
+                data = shot.attrs['style']
+                res = [int(s) for s in re.findall(r'-?\d+\.?\d*', data)] #finds all ints in positions list
                 angles.append(getAngle(res[0], res[1]))
+                data = shot.attrs['class']
+                playerids.append(data[2][2:])
                 
-        homeList = [homeTeam, distances, results, angles]
+        homeList = [homeTeam, distances, results, angles, playerids]
         
         distances = []
         results = []
         angles = []
+        playerids = []
         awayShots = soup.find(id = 'shots-{}'.format(awayTeam))
         for shot in awayShots:
             if (shot != "\n" and shot.name != 'img'):
                 info = shot.attrs['tip'].split("<br>")[1].split(" ")
                 while (len(info) > 7): #if player has middle name or suffix(ex. marvin bagley III)
                     info = info[1:]
+                while (len(info) < 7): #if player has no last name(ex. Nene)
+                    info.insert(0, 'a')
                 distances.append(info[5])
                 if (info[2] == 'made'):
                     results.append(1)
                 else:
                     results.append(0)
-                positions = shot.attrs['style']
-                res = [int(s) for s in re.findall(r'-?\d+\.?\d*', positions)]
+                data = shot.attrs['style']
+                res = [int(s) for s in re.findall(r'-?\d+\.?\d*', data)]
                 angles.append(getAngle(res[0], res[1]))
+                data = shot.attrs['class']
+                playerids.append(data[2][2:])
 
-        awayList = [awayTeam, distances, results, angles]
+        awayList = [awayTeam, distances, results, angles, playerids]
 
         return homeList + awayList
 
@@ -131,13 +143,15 @@ def getAngle(top, left):
     else:
         return np.round(math.degrees(math.atan((30-top)/(abs(250-left)))), 1) + 90
 
+#scrapeGameShots('201411010WAS')
+
 def getTeamGameShotsDataFrame(year):
 
     #get gameId list from preexisting csv
 
     gameIdList = pd.read_csv('data/eloData/team_elo_{}.csv'.format(year), index_col = 0).index
 
-    df = pd.DataFrame(index = gameIdList, columns = ['homeAbbr', 'homeDistances', 'homeResults', 'homeAngles', 'awayAbbr', 'awayDistances', 'awayResults', 'awayAngles'])
+    df = pd.DataFrame(index = gameIdList, columns = ['homeAbbr', 'homeDistances', 'homeResults', 'homeAngles', 'homePlayers', 'awayAbbr', 'awayDistances', 'awayResults', 'awayAngles', 'awayPlayers'])
     
     for id in gameIdList:
         df.loc[id] = np.asarray(scrapeGameShots(id), dtype=object)
