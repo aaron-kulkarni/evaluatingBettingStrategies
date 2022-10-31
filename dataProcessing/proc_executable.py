ec693bcef4255e7dd908001a85e597e798e1d380
Zion <jli21bh@gmail.com>
2022-10-31 15:03:00 -0400

    updated

--- dataProcessing/proc_executable.py
+++ dataProcessing/proc_executable.py
@@ -4,12 +4,15 @@ from utils.utils import *
 from TeamPerformance import * 
 from EloCalculator import *
 from cleanBettingOdds import * 
+from PerformanceMetric import * 
 
 TeamPerformance(2023).getTeamPerformanceDF(5, True)
 TeamPerformance(2023).getTeamPerformanceDF(5, False)
 concat(5, np.arange(2015,2024))
 
 convertEloCSVs.concatCSV()
+updatePerMetric(2023)
+concatAll(np.arange(2015,2024))
 
 
 #addAdjProb('../data/bettingOddsData/closing_betting_odds_2023_clean.csv').to_csv('../data/bettingOddsData/adj_prob_2023.csv')
