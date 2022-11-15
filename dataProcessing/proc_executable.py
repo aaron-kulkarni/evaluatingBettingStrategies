28a4fa7426dc4c191c5e9b6496c17a5e8a9036df
Zion <jli21bh@gmail.com>
2022-11-15 12:15:20 -0500

    ..

--- dataProcessing/proc_executable.py
+++ dataProcessing/proc_executable.py
@@ -30,16 +30,3 @@ concatAll(np.arange(2015,2024))
 
 # R script (run after raptor and odds)
 os.system('Rscript /Users/jasonli/Projects/evaluatingBettingStrategies/dataProcessing/MLE.R')
-
-years=np.arange(2015,2024)
-for year in years:
-    df = pd.read_csv('../data/gameStats/game_state_data_{}.csv'.format(year), header=[0,1], index_col=0)
-    df['gameState', 'datetime'] = pd.to_datetime(df['gameState', 'datetime'])
-    df['gameState', 'endtime'] = pd.to_datetime(df['gameState', 'endtime'])
-    df.to_csv('../data/gameStats/game_state_data_{}.csv'.format(year))
-
-# df = pd.read_csv('../data/gameStats/game_state_data_ALL.csv', header=[0,1], index_col=0)
-# df['gameState', 'datetime'] = pd.to_datetime(df['gameState', 'datetime'])
-# df['gameState', 'endtime'] = pd.to_datetime(df['gameState', 'endtime'])
-# df.to_csv('../data/gameStats/game_state_data_ALL.csv')
-
